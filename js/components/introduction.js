ab066b59bb431f1588467685db934b7e1a57853a
pkderlam87 <62060342+pkderlam87@users.noreply.github.com>
2022-03-23 13:11:46 +0100

    The scripts are in the script file. The resume is avaliable to download.

---
+++ js/components/introduction.js
@@ -0,0 +1,3 @@
+export function introText(introductionText) {
+    introductionText.innerHTML = `Hi, I am Priscila Kuhn Derlam, Junior Front End Developer based in Trondheim, Norway`;
+}
---
+++ js/script.js
@@ -0,0 +1,24 @@
+import { introText } from "./components/introduction.js";
+import { newWindowLinkedin } from "./socialMedia.js";
+
+const introduction = document.querySelector(".introduction");
+function intro() {
+    introduction.innerHTML = `<h6 class="introduction__text"></h6>
+<img src="/images/My cartoon photo.jpeg" alt="Priscila's cartoon photo" class="introduction__photo">
+<div class="introduction__buttons">
+    <div>
+        <a href="/Resume.pdf" download>
+            <button class="btn"><i class="fa-solid fa-download"></i> Resume</button>
+        </a>
+    </div>
+    <div>
+        <button class="btn linkedin"><i class="fa-brands fa-linkedin"></i> LinkedIn</button>
+    </div>
+</div>`;
+    const introductionText = document.querySelector(".introduction__text");
+    introText(introductionText);
+    //Linkedin Button in the introduction
+    const linkedin = document.querySelector(".linkedin");
+    linkedin.addEventListener("click", newWindowLinkedin);
+}
+intro();
