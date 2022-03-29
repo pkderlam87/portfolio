0839ef1c39db08b38856d4b3276cb4d6e6c280f4
pkderlam87 <62060342+pkderlam87@users.noreply.github.com>
2022-03-29 15:24:56 +0200

    Search in the page is working as Ctrl+F (show only the first result)

--- js/components/introduction.js
+++ js/components/introduction.js
@@ -1,3 +1,27 @@
-export function introText(introductionText) {
+import { newWindowLinkedin } from "../socialMedia.js";
+
+const introduction = document.querySelector(".introduction");
+
+export function introductionShow() {
+    introduction.innerHTML = `
+    <h6 class="introduction__text"></h6>
+    <img src="/images/My cartoon photo.jpeg" alt="Priscila's cartoon photo" class="introduction__photo">
+    <div class="buttons d-flex justify-content-evenly">
+        <div>
+            <a href="/Resume.pdf" download>
+                <button class="btn"><i class="fa-solid fa-download"></i> Resume</button>
+            </a>
+        </div>
+        <div>
+            <button class="btn linkedin"><i class="fa-brands fa-linkedin"></i> LinkedIn</button>
+        </div>
+    </div>`;
+    const introductionText = document.querySelector(".introduction__text");
+    introText(introductionText);
+    //Linkedin Button in the introduction
+    const linkedin = document.querySelector(".linkedin");
+    linkedin.addEventListener("click", newWindowLinkedin);
+}
+function introText(introductionText) {
     introductionText.innerHTML = `Hi, I am Priscila Kuhn Derlam, Junior Front End Developer based in Trondheim, Norway`;
 }
--- js/script.js
+++ js/script.js
@@ -1,33 +1,15 @@
-import { introText } from "./components/introduction.js";
-import { newWindowLinkedin } from "./socialMedia.js";
+import { introductionShow } from "./components/introduction.js";
 import { skillsShow } from "./components/skills.js";
 import { servicesShow } from "./components/services.js";
 import { aboutShow } from "./components/about.js";
 import { contactShow } from "./components/contact.js";
 
-const introduction = document.querySelector(".introduction");
-function intro() {
-    introduction.innerHTML = `<h6 class="introduction__text"></h6>
-<img src="/images/My cartoon photo.jpeg" alt="Priscila's cartoon photo" class="introduction__photo">
-<div class="buttons d-flex justify-content-evenly">
-    <div>
-        <a href="/Resume.pdf" download>
-            <button class="btn"><i class="fa-solid fa-download"></i> Resume</button>
-        </a>
-    </div>
-    <div>
-        <button class="btn linkedin"><i class="fa-brands fa-linkedin"></i> LinkedIn</button>
-    </div>
-</div>`;
-    const introductionText = document.querySelector(".introduction__text");
-    introText(introductionText);
-    //Linkedin Button in the introduction
-    const linkedin = document.querySelector(".linkedin");
-    linkedin.addEventListener("click", newWindowLinkedin);
-    //call the skills, services, about and contact functions
+function showPortfolioPage() {
+    //call the introduction, skills, services, about and contact functions
+    introductionShow();
     skillsShow();
     servicesShow();
     aboutShow();
     contactShow();
 }
-intro();
+showPortfolioPage();
