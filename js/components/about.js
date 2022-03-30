aa5aff44db8d3bd2f10105f7c23c7b62148fbdeb
pkderlam87 <62060342+pkderlam87@users.noreply.github.com>
2022-03-30 22:56:37 +0200

    Added style to about, skills (desktop version) and manage the buttons in the portfolio cards.

--- js/components/about.js
+++ js/components/about.js
@@ -1,6 +1,14 @@
 export function aboutShow() {
     const aboutMeShow = document.querySelector(".about__show");
-    aboutMeShow.innerHTML = `<div>
+    aboutMeShow.innerHTML = `
+    <div class="d-flex justify-content-center align-items-center col-md-5">
+    <!--This is the draft version of a video about me-->
+                    <video class="video__about__section w-100" controls muted>Your browser does not support the &lt;video&gt; tag.
+                        <source src="/images/Video_about_section.mp4" />
+                    </video>
+    </div>
+    <div class="wrap__about--info col-md-7">
+    <div>
     <p>My nickname is "Pee" (π) since I was a child, just like the name of the letter Pi when pronounced
         in Portuguese my mother language.</p>
     <p>I took a Front-end Developer higher professional degree in Norway, and I'm putting my focus on
@@ -8,9 +16,10 @@ export function aboutShow() {
     <p>I am organized, disciplined, adaptable, and I like to learn constantly. I'm always trying new
         things, I am not afraid of starting over.</p>
 </div>
-<div class="about__buttons">
-        <button class="btn p-2 about__resume__button"><a href="/Resume.pdf" download><h5><i class="fa-solid fa-download"></i> Resume</h5></a></button>
-        <button class="btn linkedin p-2"><h5><i class="fa-brands fa-linkedin"></i> LinkedIn</h5></button>
-    <button class="btn github p-2"><h5><i class="fa-brands fa-github"></i> Github</h5></button>
+<div class="about__buttons d-flex flex-column flex-md-row gap-md-3 justify-content-center">
+        <button class="btn p-sm-2 px-md-2 py-md-1 about__resume__button"><a href="/Resume.pdf" download><h5><i class="fa-solid fa-download"></i> Resume</h5></a></button>
+        <button class="btn linkedin p-sm-2 px-md-2 py-md-1"><h5><i class="fa-brands fa-linkedin"></i> LinkedIn</h5></button>
+    <button class="btn github p-sm-2 px-md-2 py-md-1"><h5><i class="fa-brands fa-github"></i> Github</h5></button>
+</div>
 </div>`;
 }
