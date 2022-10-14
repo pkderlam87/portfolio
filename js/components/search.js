eddb005996dfb915553f3b685e637e22fa7a90bf
pkderlam87 <62060342+pkderlam87@users.noreply.github.com>
2022-10-14 10:47:43 +0200

    Will add new video,searchbar working,new styles

--- js/components/search.js
+++ js/components/search.js
@@ -1,4 +1,5 @@
 const searchForm = document.querySelector("#searchForm");
+const noResults = document.querySelector(".searchbar__results");
 searchForm.addEventListener("submit", searchResult);
 
 export function searchResult(event) {
@@ -10,7 +11,9 @@ export function searchResult(event) {
         strFound = self.find(str);
         if (!strFound) {
             strFound = self.find(str, 0, 1);
+            if (!strFound) { alert("Text " + str + " not found!") }
             while (self.find(str, 0, 1)) continue;
         }
     }
+
 }
